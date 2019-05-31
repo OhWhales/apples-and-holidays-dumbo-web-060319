cfeeeb4a1ed4dd695b5a2cfd64ad705e5e76ff05
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-31 00:52:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -134,7 +134,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  finalArr=[]
+  
+  holiday_hash.each do |k,v|
+    
+  end
 end
 
 
